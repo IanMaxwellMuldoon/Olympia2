5369f561ee5941d1a477f88957f31fe7ad8c4e87
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2021-11-07 14:32:06 -0800

    Update on CalorieCounter
    I added a method to build the URL we need to fetch the data from the Food Database API

--- app/src/main/java/com/example/olympia/CalorieCounterSearch.java
+++ app/src/main/java/com/example/olympia/CalorieCounterSearch.java
@@ -3,10 +3,17 @@ package com.example.olympia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CalorieCounterSearch extends AppCompatActivity {
 
@@ -20,5 +27,11 @@ public class CalorieCounterSearch extends AppCompatActivity {
         final String URL_PREFIX = "https://api.edamam.com/api/food-database/v2/parser";
         final String API_ID = "?app_id=1ac33da3";
         final String API_KEY = "&app_key=8a5ff0e08e487166b798e56f3ab64627";
+        final String INGR = "&ingr=";
+        final String URL_SUFFIX = "&nutrition-type=logging";
+
+        String url = URL_PREFIX + API_ID + API_KEY + INGR + nameSearch + URL_SUFFIX;
+
+        
     }
-}
+    }

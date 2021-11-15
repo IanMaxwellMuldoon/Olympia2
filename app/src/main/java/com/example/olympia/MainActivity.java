fd2028bd0ea0a9bce8f55a09fc36cf80f09b532d
ColinDShepard <ColinDShepard@gmail.com>
2021-11-14 16:04:21 -0800

    Tried adding button action
    Tried linking button on home screen to calorie counter gui but then app doesn't work.

--- app/src/main/java/com/example/olympia/MainActivity.java
+++ app/src/main/java/com/example/olympia/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.olympia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-
-
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
 
     @Override
@@ -20,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
         calorie.Connect("chicken sandwich");
 
 
+    //Added something to try and open buttons but app will not open
+        
+  /*     button = (Button) findViewById(R.id.button);
+       button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    openCalorieCounter();
+            }
+        }); */
+
     }
+
+   /* public void openCalorieCounter() {
+        Intent intent = new Intent(this, CalorieCounterSearch.class);
+        startActivity(intent);
+    } */
+
+
 }

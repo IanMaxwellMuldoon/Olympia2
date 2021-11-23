4578565898127ca88bd79a85b52d8a5c3a7536a6
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2021-11-22 16:07:35 -0800

    CPO-48 #time 3h AutoComplete search suggestions
    When you type in a food the textbox has autocomplete options

--- app/src/main/java/com/example/olympia/MainActivity.java
+++ app/src/main/java/com/example/olympia/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CalorieCounterSearch calorie = new CalorieCounterSearch();
-        calorie.Connect("Hamburger");
-        calorie.connectSearch("ch");
+
 
 
     }

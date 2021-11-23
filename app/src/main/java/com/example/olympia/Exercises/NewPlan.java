55537d9abfeac431825ae685be5093332ae04b1d
Pawlo Essa <85320310+pawblastic@users.noreply.github.com>
2021-11-22 18:34:53 -0800

    CPO-43 #1h pull exercise GUI

--- app/src/main/java/com/example/olympia/Exercises/NewPlan.java
+++ app/src/main/java/com/example/olympia/Exercises/NewPlan.java
@@ -2,7 +2,10 @@ package com.example.olympia.Exercises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.olympia.R;
@@ -15,6 +18,17 @@ public class NewPlan extends AppCompatActivity {
         setContentView(R.layout.activity_new_plan);
 
         TextView newPlanText = (TextView) findViewById(R.id.newPlanText);
+        newPlanText.setText("New Plan");
 
+        Button PullExercisesButton = (Button) findViewById(R.id.PullExercisesButton);
+
+        PullExercisesButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewPlan.this, PullExercises.class);
+                startActivity(intent);
+            }
+        });
     }
 }

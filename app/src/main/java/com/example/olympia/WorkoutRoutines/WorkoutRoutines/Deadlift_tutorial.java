8a86839d402644c9af9dfbfc2f54a7b06b7353fb
Eric Aragon <50891058+ericlifts@users.noreply.github.com>
2022-02-02 12:34:44 -0800

    YouTube Tutorials
    Added youtube tutorials for squat, deadlift, bench press and shoulder press

---
+++ app/src/main/java/com/example/olympia/WorkoutRoutines/WorkoutRoutines/Deadlift_tutorial.java
@@ -0,0 +1,47 @@
+package com.example.olympia.WorkoutRoutines.WorkoutRoutines;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.olympia.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class Deadlift_tutorial extends AppCompatActivity {
+    private YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deadlift_tutorial);
+        youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                String videoId = "U3da4QV-z04";
+                youTubePlayer.loadVideo(videoId, 0); //use cueVideo to load video and thumbnail but doesnt auto play
+                 //youTubePlayerView.enterFullScreen();
+                //youTubePlayerView.exitFullScreen();
+                // youTubePlayerView.isFullScreen();
+               youTubePlayerView.toggleFullScreen();
+            }
+        });
+    }
+}
+/*  youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                super.onReady(youTubePlayer);
+                String videoId = "U3da4QV-z04";
+                youTubePlayer.loadVideo(videoId, 0); //use cueVideo to load video and thumbnail but doesnt auto play
+                // youTubePlayerView.enterFullScreen();
+                //youTubePlayerView.exitFullScreen();
+                // youTubePlayerView.isFullScreen();
+                //youTubePlayerView.toggleFullScreen();
+            }
+        }); */
---
+++ app/src/main/java/com/example/olympia/WorkoutRoutines/WorkoutRoutines/Select_tutorial.java
@@ -0,0 +1,71 @@
+package com.example.olympia.WorkoutRoutines.WorkoutRoutines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.olympia.R;
+
+public class Select_tutorial extends AppCompatActivity {
+    private Button chestButton;
+    private Button squatButton;
+    private Button deadliftButton;
+    private Button shoulderPressButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_tutorial);
+
+        chestButton = (Button) findViewById(R.id.button10);
+        squatButton = (Button) findViewById(R.id.button12);
+        deadliftButton = (Button) findViewById(R.id.button13);
+        shoulderPressButton = (Button) findViewById(R.id.button14);
+
+        chestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openChestTutorial();
+            }
+        });
+        squatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSquatTutorial();
+            }
+        });
+        deadliftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDeadliftTutorial();
+            }
+        });
+
+        shoulderPressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoulderPressTutorial();
+            }
+        });
+    }
+    public void openChestTutorial() {
+        Intent intent = new Intent(this, YouTubeLinks.class);
+        startActivity(intent);
+    }
+    public void openSquatTutorial() {
+        Intent intent = new Intent(this, Squat_tutorial.class);
+        startActivity(intent);
+    }
+    public void openDeadliftTutorial(){
+        Intent intent = new Intent(this, Deadlift_tutorial.class);
+        startActivity(intent);
+    }
+
+    public void openShoulderPressTutorial() {
+        Intent intent = new Intent(this, ShoulderPress_tutorial.class);
+        startActivity(intent);
+    }
+}

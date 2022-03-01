4cf89f3fd1fecc3df45712563410fe56bafb09b1
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2022-02-28 22:12:26 -0800

    Updated LogExercises

---
+++ app/src/main/java/com/example/olympia/Exercises/ExerciseLogButtonList.java
@@ -0,0 +1,23 @@
+package com.example.olympia.Exercises;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ExerciseLogButtonList extends Fragment {
+    private Exercise[] exercises;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/olympia/Exercises/ExerciseLogItem.java
+++ app/src/main/java/com/example/olympia/Exercises/ExerciseLogItem.java
@@ -1,5 +1,6 @@
 package com.example.olympia.Exercises;
 
 public class ExerciseLogItem {
+
     
 }

09d4023bc73ff85dc2edbc90569c9f262490448d
ColinDShepard <ColinDShepard@gmail.com>
2022-03-27 23:04:32 -0700

    CPO-78 #time 5h -m "CPO-78, CPO-63 completed"
    Completed CPO-78 and CPO-63

--- app/src/main/java/com/example/olympia/Exercises/AddExercises.java
+++ app/src/main/java/com/example/olympia/Exercises/AddExercises.java
@@ -1,22 +1,136 @@
 package com.example.olympia.Exercises;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.olympia.R;
 
+import java.util.ArrayList;
+
 public class AddExercises extends AppCompatActivity {
+    private EditText exerciseName;
+    private EditText numOfSets;
+    private EditText numOfReps;
+    private EditText weight;
+    private Button saveExercise;
+    private String name;
+    private int numSets;
+    private int numReps;
+    private int lbs;
+    private Exercise exercise;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercises);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        exerciseName = findViewById(R.id.idExerciseName);
+        exerciseName.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+
+                    exerciseName.clearFocus();
+                    exerciseName.requestFocus(exerciseName.FOCUS_DOWN);
+
+
+                }
+                return false;
+            }
+        });
+
+        numOfSets = findViewById(R.id.idNumOfSets);
+        numOfSets.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    Log.d("Message", "I made it here");
+                    numOfSets.clearFocus();
+                    numOfSets.requestFocus(numOfSets.FOCUS_DOWN);
+
+
+                }
+                return false;
+            }
+        });
+
+        numOfReps = findViewById(R.id.idNumOfReps);
+        numOfReps.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    numOfReps.clearFocus();
+                    numOfReps.requestFocus(numOfReps.FOCUS_DOWN);
+
+
+                }
+                return false;
+            }
+        });
+
+        weight = findViewById(R.id.idWeight);
+        weight.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    weight.clearFocus();
+                    weight.requestFocus(weight.FOCUS_DOWN);
+
+
+                }
+                return false;
+            }
+        });
+
+        saveExercise = findViewById(R.id.idSaveExercise);
+        saveExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = exerciseName.getText().toString();
+                numSets = Integer.parseInt(numOfSets.getText().toString());
+                numReps = Integer.parseInt(numOfReps.getText().toString());
+                lbs = Integer.parseInt(weight.getText().toString());
+
+                exercise = new Exercise(name, numSets, numReps, lbs);
+
+                Intent data = new Intent(AddExercises.this, ExerciseListFragment.class);
+
+                data.putExtra("AddExercises", exercise);
+                Log.d("Msg", "I got here");
+                Intent intent = new Intent(AddExercises.this, NewPlan.class);
+
+                Log.d("Msg", "I know I got here");
+
+                startActivity(intent);
+
+
+
+            }
+        });
+
+
+
+
+
+
 
     }
+
+
 }
--- app/src/main/java/com/example/olympia/Exercises/NewPlan.java
+++ app/src/main/java/com/example/olympia/Exercises/NewPlan.java
@@ -3,11 +3,17 @@ package com.example.olympia.Exercises;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.olympia.R;
 
 public class NewPlan extends AppCompatActivity {
+    Button addExerciseButton;
+    Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,41 @@ public class NewPlan extends AppCompatActivity {
         setContentView(R.layout.activity_new_plan);
 
         //for toolbar
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*Bundle data = getIntent().getExtras();
+
+
+        try{
+            Exercise exercise = (Exercise)data.getParcelable("AddExercises");
+            Log.d("Message", exercise.getTitle());
+        }
+        catch(NullPointerException e){
+
+        } */
+
+
+        addExerciseButton = findViewById(R.id.idAddExerciseButton);
+        addExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewPlan.this, AddExercises.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        doneButton = findViewById(R.id.idDoneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewPlan.this, PlanMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }

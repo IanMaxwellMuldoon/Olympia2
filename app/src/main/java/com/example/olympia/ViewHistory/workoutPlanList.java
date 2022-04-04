d63babec7f67357940b895dbc3ba99022c0f336d
Chad White <chad.white.428@my.csun.edu>
2022-04-04 16:45:23 -0700

    Work on retrieving data from database into app
    - Connected to and retrieved a snapshot of sample workout plan data that I added to a user in the firestore database
    - So far it successfully retrieves the data and displays it to logcat
    - Next: Need to figure out way to dynamically create android UI elements based on amount of plans store in the database

--- app/src/main/java/com/example/olympia/ViewHistory/workoutPlanList.java
+++ app/src/main/java/com/example/olympia/ViewHistory/workoutPlanList.java
@@ -1,28 +1,64 @@
 package com.example.olympia.ViewHistory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.olympia.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class workoutPlanList extends AppCompatActivity {
     // Variables
-    TextView planName, planData;
+    TextView planName, planDate;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_history);
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        // Hooks
+        planName = findViewById(R.id.planName);
+        planDate = findViewById(R.id.planDate);
+
         //for toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("History");
+
+        // Get data from Firestore Database to populate workout plan list
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("users").document("1IiJknuI0cW3GnRI2sCII9i6KSR2").collection("workoutData").document("plan1");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        Log.d("Document", doc.getData().toString());
+                    } else {
+                        Log.d("Document", "No Data");
+                    }
+                }
+            }
+        });
     }
 }

17a6248c461ea8c159fae3d57aaefcee3bd454a1
Pawlo Essa <85320310+pawblastic@users.noreply.github.com>
2022-04-16 22:03:44 -0700

    Calendar view

---
+++ app/src/main/java/com/example/olympia/ViewHistory/CalendarView.java
@@ -0,0 +1,26 @@
+package com.example.olympia.ViewHistory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.olympia.R;
+
+public class CalendarView extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_view);
+
+//        calendarView = (CalendarView) findViewById(R.id.CalendarView);
+//        myDate= (TextView) findViewById(R.id.idDate);
+
+
+        
+    }
+}
